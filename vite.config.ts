6861119edf771c1eeb65f36100ab6c8071e6fd47
couchcards <pat@couchcards.com>
2026-03-30 15:35:12 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(() => ({
-  base: '/nightlight.llc/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
